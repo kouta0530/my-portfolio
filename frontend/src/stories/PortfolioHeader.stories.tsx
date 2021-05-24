b4a23468fcba55729efea34dbf502ccb39cb3fe8
kouta0530 <16jk047@ymklab.net>
2021-05-24 20:21:33 +0900

    feat(header): PortfolioHeaderコンポーネントのstoryの追加

---
+++ frontend/src/stories/PortfolioHeader.stories.tsx
@@ -0,0 +1,42 @@
+import { Story, Meta } from "@storybook/react";
+import {
+  PortfolioHeader,
+  HeaderProps,
+} from "../components/PortfolioHeader/PortfolioHeader";
+import NavigationElements from "../models/NavigationElement";
+
+const mockNavigationElements = [
+  {
+    elementName: "testElement",
+    elemenUrl: "",
+  },
+  {
+    elementName: "testElement2",
+    elementUrl: "",
+  },
+] as NavigationElements[];
+
+export default {
+  title: "Example/PortfolioHeader",
+  component: PortfolioHeader,
+  argTypes: {
+    headerTitle: { control: "text" },
+    navigationElements: {
+      types: "array",
+    },
+  },
+} as Meta;
+
+const Template: Story<HeaderProps> = (args) => <PortfolioHeader {...args} />;
+
+export const Title = Template.bind({});
+Title.args = {
+  headerTitle: "Title",
+  navigationElements: [],
+};
+
+export const Elements = Template.bind({});
+Elements.args = {
+  headerTitle: "",
+  navigationElements: mockNavigationElements,
+};
