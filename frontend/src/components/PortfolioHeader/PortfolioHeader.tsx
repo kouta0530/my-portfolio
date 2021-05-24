64df01fae7ede9eab00fb999fe089c9baeeac87f
kouta0530 <16jk047@ymklab.net>
2021-05-24 20:17:31 +0900

    feat(header): PortfolioHeaderコンポーネントの作成

---
+++ frontend/src/components/PortfolioHeader/PortfolioHeader.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import NavigationElement from "../../models/NavigationElement";
+import "./style.scss";
+
+export interface HeaderProps {
+  headerTitle: string;
+  navigationElements: NavigationElement[];
+}
+
+export class PortfolioHeader extends React.Component<HeaderProps> {
+  render() {
+    return (
+      <>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h3">{this.props.headerTitle}</Typography>
+          </Toolbar>
+          <Toolbar>
+            <ul>
+              {this.props.navigationElements.map((e) => {
+                return <li>{e.elementName}</li>;
+              })}
+            </ul>
+          </Toolbar>
+        </AppBar>
+      </>
+    );
+  }
+}
