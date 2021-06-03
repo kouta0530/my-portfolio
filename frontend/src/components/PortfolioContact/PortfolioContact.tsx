9c8b12332e715119547cd97a0775dab5d294f758
kouta0530 <16jk047@ymklab.net>
2021-06-03 11:25:49 +0900

    feat(components): 各コンテンツのアイコンを横並びで表示することができる

--- frontend/src/components/PortfolioContact/PortfolioContact.tsx
+++ frontend/src/components/PortfolioContact/PortfolioContact.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import IconParameter from "../../models/IconParameter";
 import Typography from "@material-ui/core/Typography";
+import "./style.scss";
 
 type ContactPrams = {
   contactHeaderTitle: String;
@@ -12,8 +13,13 @@ export default class PortfolioContact extends React.Component<ContactPrams> {
   render() {
     return (
       <>
-        <div>
+        <div className="contact-title">
           <Typography variant="h4">{this.props.contactHeaderTitle}</Typography>
+        </div>
+        <div className="content-icons">
+          {this.props.contentIcons.map((e) => (
+            <img src={e.iconImagePath} alt={e.iconName} />
+          ))}
         </div>
       </>
     );
