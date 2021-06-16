8c3c1536f25907ecf2ece7994301e54fb256b261
kouta0530 <16jk047@ymklab.net>
2021-06-16 22:08:54 +0900

    feat(components): Skillコンポーネントの作成

---
+++ frontend/src/components/PortfolioSkill/PortfolioSkill.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SkillParameter from "../../models/SkillParameter";
+import StarIcon from "@material-ui/icons/Star";
+import "./style.scss";
+
+export interface SkillProp {
+  skillInformation: SkillParameter;
+}
+
+export const PortfolioSkill: React.FC<SkillProp> = ({ ...props }) => {
+  const stars = Array(props.skillInformation.skilledStars).fill(<StarIcon />);
+
+  return (
+    <>
+      <div className="skill">
+        <label>{props.skillInformation.skillLabel}</label>
+        <div className="skilled-stars">
+          <span className="skilled-star">{stars}</span>
+        </div>
+      </div>
+    </>
+  );
+};
