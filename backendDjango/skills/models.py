7b228382f8a02c506d6653aa8ec56b77440c68fc
kouta0530 <16jk047@ymklab.net>
2021-07-02 23:18:34 +0900

    feat(django-skills): Skillモデルの作成

--- backendDjango/skills/models.py
+++ backendDjango/skills/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Skill(models.Model):
+    skillLabel = models.CharField(max_length=200)
+    skilledStars = models.IntegerField(default=1)
+    skillType = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.skillLabel
