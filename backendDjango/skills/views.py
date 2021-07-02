e3053af20e06d0b683b1121a9624138173f51d2b
kouta0530 <16jk047@ymklab.net>
2021-07-02 23:19:02 +0900

    feat(django-skills): /v1/skillsAPIの実装

--- backendDjango/skills/views.py
+++ backendDjango/skills/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 # Create your views here.
+from django.core import serializers
+from .models import Skill
+
+
+def index(request):
+    skillInformations = Skill.objects.all()
+    response = serializers.serialize('json', skillInformations)
+    return HttpResponse(response, content_type="text/json")
