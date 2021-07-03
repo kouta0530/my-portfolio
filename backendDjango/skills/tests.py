9b56e70181e038c13ed0af3e250f2a7c8bd49f7a
kouta0530 <16jk047@ymklab.net>
2021-07-03 19:30:40 +0900

    feat(django-skills): /v1/skillsのレスポンスが正しく受け取れるかテスト

--- backendDjango/skills/tests.py
+++ backendDjango/skills/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from .models import Skill
 # Create your tests here.
+
+
+class SkillsApiTest(TestCase):
+
+    def canGetSkillInformationList(self):
+        reponse = self.client.get('/v1/skills')
+        self.assertEqual(reponse.status_code, 200)
