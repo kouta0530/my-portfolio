c2b546c15920dc0b7d2546520f0a705ae5fd162c
kouta0530 <16jk047@ymklab.net>
2021-07-03 23:07:14 +0900

    feat(django-skills): /v1/skills/:idのレスポンステスト

--- backendDjango/skills/tests.py
+++ backendDjango/skills/tests.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Skill
+
 # Create your tests here.
 
 
 class SkillsApiTest(TestCase):
 
-    def canGetSkillInformationList(self):
-        reponse = self.client.get('/v1/skills')
+    def test_canGetSkillInformationList(self):
+        reponse = self.client.get('/v1/skills/')
         self.assertEqual(reponse.status_code, 200)
+
+    def test_canGetSkillInformation(self):
+        response = self.client.get('/v1/skills/?id=1')
+        self.assertEqual(response.status_code, 200)
+        skill = Skill.objects.filter(pk=1)
