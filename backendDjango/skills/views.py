d53290cdc2c61e260df422143f2d13327a465bd9
kouta0530 <16jk047@ymklab.net>
2021-07-03 10:50:08 +0900

    feat(django-skills): IDに対応したSkillモデルを返すAPIを追加

--- backendDjango/skills/views.py
+++ backendDjango/skills/views.py
@@ -3,9 +3,19 @@ from django.http.response import HttpResponse
 # Create your views here.
 from django.core import serializers
 from .models import Skill
+import json
 
 
 def index(request):
     skillInformations = Skill.objects.all()
     response = serializers.serialize('json', skillInformations)
     return HttpResponse(response, content_type="text/json")
+
+
+def getPortfolioSkill(request, id):
+    skillInformation = Skill.objects.filter(pk=id).values(
+        'skillLabel', 'skilledStars', 'skillType')
+
+    return HttpResponse(
+        json.dumps(list(skillInformation), indent=2, ensure_ascii=False),
+        content_type="text/json")
