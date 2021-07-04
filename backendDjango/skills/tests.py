9e37d9bae0fafa338d49d6f7d0e72b30c6f8f40a
kouta0530 <16jk047@ymklab.net>
2021-07-04 23:47:10 +0900

    feat(django-skills): テストデータを用意する処理を追加

--- backendDjango/skills/tests.py
+++ backendDjango/skills/tests.py
@@ -6,6 +6,10 @@ from .models import Skill
 
 
 class SkillsApiTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Skill.objects.create(
+            skill_label="Django", skilled_stars=2, skill_type="バックエンド")
 
     def test_can_get_skillinformationlist(self):
         response = self.client.get('/v1/skills/')
@@ -20,9 +24,13 @@ class SkillsApiTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_create_skill_model(self):
-        skill = Skill.objects.create(
-            skill_label="Django", skilled_stars=2, skill_type="バックエンド")
+        skill = Skill.objects.get(pk=1)
 
+        self.assertEqual(skill.pk, 1)
         self.assertEqual(skill.skill_label, "Django")
         self.assertEqual(skill.skilled_stars, 2)
         self.assertEqual(skill.skill_type, "バックエンド")
+
+    def test_skilled_stars_default(self):
+        skill = Skill.objects.create(skill_label="Django", skill_type="バックエンド")
+        self.assertEqual(skill.skilled_stars, 1)
