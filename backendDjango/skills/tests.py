7ae89921a3927631cb2670342369fefa35df302f
kouta0530 <16jk047@ymklab.net>
2021-07-04 23:22:28 +0900

    feat(django-skills): Skillモデルが正しく作成されているかテスト

--- backendDjango/skills/tests.py
+++ backendDjango/skills/tests.py
@@ -18,3 +18,11 @@ class SkillsApiTest(TestCase):
     def test_can_get_skill_category(self):
         response = self.client.get('/v1/skills/type')
         self.assertEqual(response.status_code, 200)
+
+    def test_create_skill_model(self):
+        skill = Skill.objects.create(
+            skill_label="Django", skilled_stars=2, skill_type="バックエンド")
+
+        self.assertEqual(skill.skill_label, "Django")
+        self.assertEqual(skill.skilled_stars, 2)
+        self.assertEqual(skill.skill_type, "バックエンド")
