962f13517e79d2718f6b9cbff9c4322446781828
kouta0530 <16jk047@ymklab.net>
2021-07-09 08:47:24 +0900

    fix(story): mockData生成用のメソッドを用意

--- frontend/src/stories/PortfolioWorkGroup.stories.tsx
+++ frontend/src/stories/PortfolioWorkGroup.stories.tsx
@@ -1,10 +1,27 @@
 import { Story, Meta } from "@storybook/react";
 import { PortfolioWorkGroup, WorkGroupProp } from "../components/PortfolioWorkGroup/PortfolioWorkGroup";
+import IconParameter from "../models/IconParameter";
+import { WorkParameter } from "../models/WorkParameter";
 
 export default {
   title: 'Example/PortfolioWork/PortfolioWorkGroup',
   component: PortfolioWorkGroup
 } as Meta
+
+const createMockApplication　= (
+  name: string, 
+  applicationPictureUrl?: string,
+  applicationDescription?: string,
+  applicationOptions?: IconParameter[]
+)=> {
+
+    return {
+      applicationName: name,
+      applicationPictureUrl: applicationPictureUrl,
+      applicationDescription: applicationDescription,
+      applicationOptions: applicationOptions
+    } as WorkParameter
+}
 
 const Template: Story<WorkGroupProp> = (args) => <PortfolioWorkGroup {...args} />
 
@@ -12,15 +29,7 @@ export const Default = Template.bind({})
 
 Default.args = {
   applications:[
-    {
-      applicationName:"testSampleApplication",
-      applicationPictureUrl:'./images/Screenshot from 2021-06-09 13-20-09.png',
-      applicationDescription: 'これはテスト用の説明です。',
-    },
-    {
-      applicationName:"testReactApplication",
-      applicationPictureUrl:'./logo192.png',
-      applicationDescription: 'これはテスト用の説明です。',
-    }
+    createMockApplication('testSampleApplication','/images/Screenshot from 2021-06-09 13-20-09.png','これはテスト用の説明です。'),
+    createMockApplication('testReactApplication','./logo192.png','これはテスト用の説明です。')
   ]
 }
