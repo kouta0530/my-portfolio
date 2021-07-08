db13f7848fedfddbd32604c734ab370814dedfe8
kouta0530 <16jk047@ymklab.net>
2021-07-08 15:25:34 +0900

    feat(story): WorkコンポーネントのDefaultのStoryを作成

---
+++ frontend/src/stories/PortfolioWork.stories.tsx
@@ -0,0 +1,28 @@
+import { Story, Meta  } from "@storybook/react";
+import { PortfolioWork,WorkProp } from "../components/PortfolioWork/PortfolioWork";
+
+export default {
+  title: 'Example/PortfolioWork',
+  component: PortfolioWork
+} as Meta
+
+const Template: Story<WorkProp> = (args) => <PortfolioWork {...args} />
+
+export const Default = Template.bind({})
+Default.args ={
+    applicationName:"testSampleApplication",
+    applicationPictureUrl:'./images/Screenshot from 2021-06-09 13-20-09.png',
+    applicationDescription: 'これはテスト用の説明です。',
+    applicationOptions:[
+      {
+        iconName:'',
+        iconImagePath:'logo192.png',
+        contentUrl:'',
+      },
+      {
+        iconName:'',
+        iconImagePath:'logo192.png',
+        contentUrl:'',
+      }
+    ]
+}
