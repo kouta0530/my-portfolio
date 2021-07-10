5936f87d2a7e9d50347f1b9020ae29d7e2874044
kouta0530 <16jk047@ymklab.net>
2021-07-10 20:11:17 +0900

    feat(django-works): Workモデルの作成

--- backendDjango/works/models.py
+++ backendDjango/works/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+class Work(models.Models):
+    application_name = models.CharField(max_length=30,blank=False,null=False)
+    
+    application_description = models.TextField(max_length=400,default="",blank=True,null=False)
+    
+    http_regix = RegexValidator(regex=r"https?://[\w!?/+\-_~;:.,*&@#$%()'[\]]+")
+    application_picture_url = models.CharField(max_length=200,default="",blank=True,null=False,validators=[http_regix])
