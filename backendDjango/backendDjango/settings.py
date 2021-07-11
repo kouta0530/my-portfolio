0b9793f4ba89d463501f2880af5c15629da9edcb
kouta0530 <16jk047@ymklab.net>
2021-07-11 17:56:54 +0900

    feat(django-works): Workモデルのマイグレーション

--- backendDjango/backendDjango/settings.py
+++ backendDjango/backendDjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'works.apps.WorksConfig',
     'skills.apps.SkillsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- backendDjango/works/models.py
+++ backendDjango/works/models.py
@@ -3,7 +3,7 @@ from django.core.validators import URLValidator
 
 # Create your models here.
 
-class Work(models.Models):
+class Work(models.Model):
     application_name = models.CharField(max_length=30,blank=False,null=False)
     
     application_description = models.TextField(max_length=400,default="",blank=True,null=False)
