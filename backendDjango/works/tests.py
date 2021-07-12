71d337c17f2a480154722c4cdd7f256337da6a70
kouta0530 <16jk047@ymklab.net>
2021-07-12 11:26:56 +0900

    feat(djanbo-works): workモデルのapplication_nameのカラム名のテスト

--- backendDjango/works/tests.py
+++ backendDjango/works/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Work
 
 # Create your tests here.
+
+class WorkApiTest(TestCase):
+  @classmethod
+  def setUpTestData(cls):
+    Work.objects.create(
+      application_name="testApplication",
+      application_description="eeeeeeeeeeeeeeeeeeeeeeeee",
+      application_picture_url="https://example.com"
+    )
+
+  def test_application_name_label(self):
+    work = Work.objects.get(pk=1)
+    self.assertEquals(work._meta.get_field('application_name').verbose_name,'application name')
