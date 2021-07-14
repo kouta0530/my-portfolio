76a801878d32a19dd5b0e2abc8a75f6dbf81f28c
kouta0530 <16jk047@ymklab.net>
2021-07-15 08:47:39 +0900

    fix(django-works): 非推奨なメソッドを修正

--- backendDjango/works/tests.py
+++ backendDjango/works/tests.py
@@ -16,17 +16,17 @@ class WorkApiTest(TestCase):
 
     def test_application_name_label(self):
         work = Work.objects.get(pk=1)
-        self.assertEquals(work._meta.get_field(
+        self.assertEqual(work._meta.get_field(
             'application_name').verbose_name, 'application name')
 
     def test_application_description_label(self):
         work = Work.objects.get(pk=1)
-        self.assertEquals(work._meta.get_field(
+        self.assertEqual(work._meta.get_field(
             'application_description').verbose_name, 'application description')
 
     def test_application_picture_url_label(self):
         work = Work.objects.get(pk=1)
-        self.assertEquals(work._meta.get_field(
+        self.assertEqual(work._meta.get_field(
             'application_picture_url').verbose_name, 'application picture url')
 
     def test_application_picture_url_validation_error(self):
@@ -36,19 +36,19 @@ class WorkApiTest(TestCase):
 
     def test_application_name_max_length(self):
         work = Work.objects.get(pk=1)
-        self.assertEquals(work._meta.get_field(
+        self.assertEqual(work._meta.get_field(
             'application_name').max_length, 30)
 
     def test_application_description_max_length(self):
         work = Work.objects.get(pk=1)
-        self.assertEquals(work._meta.get_field(
+        self.assertEqual(work._meta.get_field(
             'application_description').max_length, 400)
 
     def test_application_picture_url_max_length(self):
         work = Work.objects.get(pk=1)
-        self.assertEquals(work._meta.get_field(
+        self.assertEqual(work._meta.get_field(
             'application_picture_url').max_length, 200)
 
     def test_application_description_default(self):
         no_description_text_work = Work.objects.create(application_name="test")
-        self.assertEquals(no_description_text_work.application_description, "")
+        self.assertEqual(no_description_text_work.application_description, "")
