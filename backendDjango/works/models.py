ddf16054664a920dd62e4615d01e8613112bdf0d
kouta0530 <16jk047@ymklab.net>
2021-07-15 09:19:26 +0900

    feat(django-works): workOptionモデルの作成

--- backendDjango/works/models.py
+++ backendDjango/works/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.core.validators import URLValidator
-
+from django.core.validators import URLValidator, RegexValidator
 # Create your models here.
 
 
@@ -13,3 +12,14 @@ class Work(models.Model):
     http_regix = URLValidator(schemes=['http', 'https'])
     application_picture_url = models.CharField(
         max_length=200, default="", blank=True, null=False, validators=[http_regix])
+
+
+class WorkOption(models.Model):
+    work = models.ForeignKey(Work, on_delete=models.CASCADE)
+    icon_name = models.CharField(max_length=30)
+    contents_url = models.TextField(
+        max_length=2000, validators=[URLValidator()])
+
+    image_validator = RegexValidator(r'^./[/.*/]*.+.[png|jpg]$')
+    icon_image_path = models.CharField(
+        max_length=200, validators=[image_validator])
