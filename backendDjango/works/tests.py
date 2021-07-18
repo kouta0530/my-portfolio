2dfaac520e762941518e81169dd99541e098543c
kouta0530 <16jk047@ymklab.net>
2021-07-18 14:32:53 +0900

    feat(djano-works): icon_image_pathのバリデーションが適切であるかテスト

--- backendDjango/works/tests.py
+++ backendDjango/works/tests.py
@@ -101,6 +101,13 @@ class WorkApiTest(TestCase):
         with self.assertRaisesMessage(ValidationError, 'Enter a valid URL.'):
             worng_content_url_work_option.full_clean()
 
+    def test_icon_image_path_validation(self):
+        options = WorkOption.objects.get(pk=1)
+        options.icon_image_path = "d/test.png"
+
+        with self.assertRaisesMessage(ValidationError, 'Enter a valid value.'):
+            options.full_clean()
+
     def test_get_parent_model_from_child(self):
         child = WorkOption.objects.select_related().get(pk=1)
         self.assertEqual(child.work, Work.objects.get(pk=1))
