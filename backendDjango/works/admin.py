45c6b455497f6c12dfcbd265b198269d9a06a94f
kouta0530 <16jk047@ymklab.net>
2021-07-19 18:22:51 +0900

    feat(django-works): Workモデルのadminからオプションを編集できるようにする

--- backendDjango/works/admin.py
+++ backendDjango/works/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Work, WorkOption
 # Register your models here.
-admin.site.register(Work)
-admin.site.register(WorkOption)
+
+
+class WorkOptionInline(admin.StackedInline):
+    model = WorkOption
+    extra = 3
+
+
+class WorkAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Required', {"fields": ['application_name']}),
+        ('Optional', {"fields": ['application_description',
+                                 'application_picture_url']}),
+    )
+    inlines = [WorkOptionInline]
+
+
+admin.site.register(Work, WorkAdmin)
