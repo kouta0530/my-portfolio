23af350a86976af141df4b3bb2bfe24ba0163853
kouta0530 <16jk047@ymklab.net>
2021-07-23 23:17:13 +0900

    feat(pages): mainPageにSkillGroupコンポーネントを追加

--- frontend/src/pages/PortfolioMainPage/PortfolioMainPage.tsx
+++ frontend/src/pages/PortfolioMainPage/PortfolioMainPage.tsx
@@ -1,9 +1,25 @@
-import React from "react";
-import { PortfolioHeader } from "../../components/PortfolioHeader/PortfolioHeader";
-import { PortfolioProfile } from "../../components/PortfolioProfile/PortfolioProfile";
-import "./style.scss";
+import React from 'react';
+import { PortfolioHeader } from '../../components/PortfolioHeader/PortfolioHeader';
+import { PortfolioProfile } from '../../components/PortfolioProfile/PortfolioProfile';
+import { PortfolioSkillGroup } from '../../components/PortfolioSkillGroup/PortfolioSkillGroup';
+import './style.scss';
+import { getPortfolioSkillList } from '../../api/portfolioSkillsAPI';
+import SkillParameter from '../../models/SkillParameter';
 
-export default class PortfolioMainPage extends React.Component {
+export default class PortfolioMainPage extends React.Component<{}, any> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { skills: new Array<SkillParameter>(0), skillTypes: new Array<String>(0) };
+  }
+
+  async componentDidMount() {
+    const res = await getPortfolioSkillList();
+    if (typeof res === 'object') {
+      this.setState({ skills: res, skillTypes: Array.from(new Set(res.map((e) => e.skillType))) });
+    }
+    console.log(this.state);
+  }
+
   render() {
     return (
       <>
@@ -11,10 +27,10 @@ export default class PortfolioMainPage extends React.Component {
           <PortfolioHeader
             headerTitle="my-portfolio"
             navigationElements={[
-              { elementName: "profile", elementUrl: "" },
-              { elementName: "skill", elementUrl: "" },
-              { elementName: "work", elementUrl: "" },
-              { elementName: "contact", elementUrl: "" },
+              { elementName: 'profile', elementUrl: '' },
+              { elementName: 'skill', elementUrl: '' },
+              { elementName: 'work', elementUrl: '' },
+              { elementName: 'contact', elementUrl: '' },
             ]}
           />
         </header>
@@ -22,17 +38,18 @@ export default class PortfolioMainPage extends React.Component {
           <PortfolioProfile
             myName="saki"
             myPicture={{
-              iconName: "mypicture",
-              contentUrl: "",
-              iconImagePath: "",
+              iconName: 'mypicture',
+              contentUrl: '',
+              iconImagePath: '',
             }}
             myHobbyList={[
               {
-                hobbyName: "カラオケ",
-                hobbiesExampleInformation: ["ポルノグラフィティ"],
+                hobbyName: 'カラオケ',
+                hobbiesExampleInformation: ['ポルノグラフィティ'],
               },
             ]}
           />
+          <PortfolioSkillGroup skillTypes={this.state.skillTypes} skillInformation={this.state.skills} />
         </div>
       </>
     );
