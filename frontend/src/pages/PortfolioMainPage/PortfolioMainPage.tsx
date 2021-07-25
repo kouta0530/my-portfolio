6d2bb38a298a283296cc97f71915bcab60fd98c1
kouta0530 <16jk047@ymklab.net>
2021-07-25 11:32:25 +0900

    feat(pages): mainページにWorkGroupコンポーネントを追加

--- frontend/src/pages/PortfolioMainPage/PortfolioMainPage.tsx
+++ frontend/src/pages/PortfolioMainPage/PortfolioMainPage.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { PortfolioHeader } from '../../components/PortfolioHeader/PortfolioHeader';
 import { PortfolioProfile } from '../../components/PortfolioProfile/PortfolioProfile';
 import { PortfolioSkillGroup } from '../../components/PortfolioSkillGroup/PortfolioSkillGroup';
+import { PortfolioWorkGroup } from '../../components/PortfolioWorkGroup/PortfolioWorkGroup';
 import './style.scss';
 import { getPortfolioSkillList } from '../../api/portfolioSkillsAPI';
 import SkillParameter from '../../models/SkillParameter';
+import { getPortfolioWorks } from '../../api/portfolioWorksAPI';
+import { WorkParameter } from '../../models/WorkParameter';
 
 export default class PortfolioMainPage extends React.Component<{}, any> {
   constructor(props: {}) {
     super(props);
-    this.state = { skills: new Array<SkillParameter>(0), skillTypes: new Array<String>(0) };
+    this.state = {
+      skills: new Array<SkillParameter>(0),
+      skillTypes: new Array<String>(0),
+      works: new Array<WorkParameter>(0),
+    };
   }
 
   async componentDidMount() {
@@ -17,7 +24,9 @@ export default class PortfolioMainPage extends React.Component<{}, any> {
     if (typeof res === 'object') {
       this.setState({ skills: res, skillTypes: Array.from(new Set(res.map((e) => e.skillType))) });
     }
-    console.log(this.state);
+
+    const works = await getPortfolioWorks();
+    this.setState({ works: works });
   }
 
   render() {
@@ -35,21 +44,31 @@ export default class PortfolioMainPage extends React.Component<{}, any> {
           />
         </header>
         <div className="container">
-          <PortfolioProfile
-            myName="saki"
-            myPicture={{
-              iconName: 'mypicture',
-              contentUrl: '',
-              iconImagePath: '',
-            }}
-            myHobbyList={[
-              {
-                hobbyName: 'カラオケ',
-                hobbiesExampleInformation: ['ポルノグラフィティ'],
-              },
-            ]}
-          />
-          <PortfolioSkillGroup skillTypes={this.state.skillTypes} skillInformation={this.state.skills} />
+          <div>
+            <h2>Profile</h2>
+            <PortfolioProfile
+              myName="saki"
+              myPicture={{
+                iconName: 'mypicture',
+                contentUrl: '',
+                iconImagePath: '',
+              }}
+              myHobbyList={[
+                {
+                  hobbyName: 'カラオケ',
+                  hobbiesExampleInformation: ['ポルノグラフィティ'],
+                },
+              ]}
+            />
+          </div>
+          <div>
+            <h2>Skill</h2>
+            <PortfolioSkillGroup skillTypes={this.state.skillTypes} skillInformation={this.state.skills} />
+          </div>
+          <div>
+            <h2>Works</h2>
+            <PortfolioWorkGroup applications={this.state.works} />
+          </div>
         </div>
       </>
     );
